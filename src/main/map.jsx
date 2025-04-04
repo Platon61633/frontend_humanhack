c56110f35c057e0d50247c9beed61ed486fefea7
Platon61633 <peredreevplaton@gmail.com>
2025-04-04 16:44:56 +0300

    ++

--- src/main/map.jsx
+++ src/main/map.jsx
@@ -9,9 +9,8 @@ const Map = () => {
     const position = [51.505, -0.09]
 
     return(
-        // <div className='Map'>
-            {/* <Header/> */}
-        <div>
+        <div className='Map'>
+          <Header/>
           <MapContainer center={position} zoom={13} >
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
